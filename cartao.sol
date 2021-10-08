79aad978e848474c6e24396473bd6d225c688b16
Felipe Leonardo Rodrigues <92129736+atas26@users.noreply.github.com>
2021-10-07 21:41:27 -0300

    Criação cartao.sol

---
+++ cartao.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.8.9;
+
+contract CartaodeVacinacao {
+    //autor: Felipe
+    string public constant nome = "Felipe Leonardo Rodrigues";
+    string public unidade = "UBS 2";
+    uint public CNES = 10213;
+    uint public Data = 20211007;
+    uint private lote = 123;
+    string constant private fabricante = "Astrazenica";
+    string public vacinador = "Leonardo";
+    bool public imunizada = false;
+    address public wallet = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    
+    
+}
